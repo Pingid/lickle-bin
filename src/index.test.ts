d95c6e81cf8717c085b4c9cb1dda8bcd8fd3d72a
Dan Beaven <dm.beaven@gmail.com>
2025-05-31 12:28:19 +0100

    initial

---
+++ src/index.test.ts
@@ -0,0 +1,59 @@
+import { it, expect } from 'vitest'
+
+import * as b from './index.js'
+
+export const test = b.struct({
+  uint8: b.uint8(),
+  uint16: b.uint16(),
+  uint32: b.uint32(),
+  int8: b.int8(),
+  int16: b.int16(),
+  int32: b.int32(),
+  float32: b.float32(),
+  float64: b.float64(),
+  bigInt64: b.bigInt64(),
+  bigUint64: b.bigUint64(),
+
+  utf8: b.utf8(),
+  utf8Fixed: b.utf8({ fixed: 3 }),
+  optional1: b.optional(b.utf8()),
+  optional2: b.optional(b.utf8()),
+
+  array: b.array(b.utf8()),
+  tuple: b.tuple(b.uint8<1>(), b.uint16<2>(), b.uint32<3>()),
+
+  union: b.union({
+    one: b.struct({ type: b.uint8(), name: b.utf8() }),
+    two: b.array(b.uint8()),
+  }),
+
+  descriminatedUnion: b.descriminatedUnion('tag', [
+    b.struct({ tag: b.literal('a'), a: b.utf8() }),
+    b.struct({ tag: b.literal('b'), b: b.utf8() }),
+  ]),
+})
+
+it('should encode and decode', () => {
+  const value: b.TypeOf<typeof test>['encode'] = {
+    uint8: 10,
+    uint16: 10,
+    uint32: 10,
+    int8: 10,
+    int16: 10,
+    int32: 10,
+    float32: 10,
+    float64: 10,
+    bigInt64: 10n,
+    bigUint64: 10n,
+    utf8: 'Some foo some bar',
+    utf8Fixed: '123',
+    optional2: '456',
+    array: ['test', 'test2'],
+    tuple: [1, 2, 3],
+    union: ['two', [1]],
+    descriminatedUnion: { tag: 'a', a: 'foo' },
+  }
+  const encoded = b.encode(test, value)
+  const decoded = b.decode(test, encoded)
+  expect(decoded).toEqual(value)
+})
